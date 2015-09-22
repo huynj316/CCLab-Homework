12de0db7b54880a0f4609dd81a3f6a6c90834bfb
Jourdan Bousfield <jourdanbousfield@gmail.com>
2015-09-22 17:39:12 -0400

    Homework Week 3

---
+++ Jourdan Bousfield/Week3Homework/js/flickr.js
@@ -0,0 +1,45 @@
+(function(document, window) {
+   'use strict';
+
+   var apiKey = 'cbd88f2ea10142adf52b48be1ef96e84';
+   var apiURL = 'https://api.flickr.com/services/rest/';
+
+   function searchText(parameters) {
+      var requestParameters = Utility.extend(parameters, {
+         method: 'flickr.photos.search',
+         api_key: apiKey,
+         format: 'json'
+      });
+
+      var script = document.createElement('script');
+      script.src = Utility.buildUrl(apiURL, requestParameters);
+      document.head.appendChild(script);
+      document.head.removeChild(script);
+   }
+
+   function buildThumbnailUrl(photo) {
+      return 'https://farm' + photo.farm + '.staticflickr.com/' + photo.server +
+      '/' + photo.id + '_' + photo.secret + '_q.jpg';
+   }
+
+   function buildPhotoUrl(photo) {
+      return 'https://farm' + photo.farm + '.staticflickr.com/' + photo.server +
+             '/' + photo.id + '_' + photo.secret + '.jpg';
+   }
+
+   function buildPhotoLargeUrl(photo) {
+      return 'https://farm' + photo.farm + '.staticflickr.com/' + photo.server +
+      '/' + photo.id + '_' + photo.secret + '_b.jpg';
+
+   }
+
+
+   window.Flickr = Utility.extend(window.Flickr || {}, {
+      buildThumbnailUrl: buildThumbnailUrl,
+      buildPhotoUrl: buildPhotoUrl,
+      buildPhotoLargeUrl: buildPhotoLargeUrl,
+      searchText: searchText
+   });
+})(document, window);
+
+// Sourced from Aurelio De Rosa, "Creating an Image Gallery with the Flickr API — Requirements and Markup" Feb 16, 2015
